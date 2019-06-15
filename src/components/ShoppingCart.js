8062202f5420a8c61e133fbd4d98e7a75a7e2ab4
Nicolas Pineros <ndpinerosp@unal.edu.co>
2019-06-15 01:16:40 +0000

    first commit

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Panel, Table, Button, Glyphicon } from 'react-bootstrap';
-import store from '../store'; //imp
+import {removeFromCart} from '../ActionCreators';
+import { connect } from 'react-redux';
+
 const styles = {
   footer: {
     fontWeight: 'bold'
@@ -8,24 +10,6 @@ const styles = {
 }
 
 class ShoppingCart extends Component {
-  constructor() {
-    super();
-    this.removeFromCart = this.removeFromCart.bind(this);
-
-    this.state = {
-      cart: []
-    }
-    
-    //suscribirse a los cambios que ocurran en el store
-    //actualizando el estado
-    store.subscribe(() => {
-      this.setState({
-        cart: store.getState().cart
-      });
-    });
-
-  }
-
   
 
   render() {
@@ -33,18 +17,18 @@ class ShoppingCart extends Component {
       <Panel header="Shopping Cart">
         <Table fill>
           <tbody>
-            {this.state.cart.map(product =>
+            {this.props.cart.map(product =>
               <tr key={product.id}>
                 <td>{product.name}</td>
                 <td className="text-right">${product.price}</td>
-                <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => this.removeFromCart(product)}><Glyphicon glyph="trash" /></Button></td>
+                <td className="text-right"><Button bsSize="xsmall" bsStyle="danger" onClick={() => this.props.removeFromCart(product)}><Glyphicon glyph="trash" /></Button></td>
               </tr>
             )}
           </tbody>
           <tfoot>
             <tr>
               <td colSpan="4" style={styles.footer}>
-                Total: ${this.state.cart.reduce((sum, product) => sum + product.price, 0)}
+                Total: ${this.props.cart.reduce((sum, product) => sum + product.price, 0)}
               </td>
             </tr>
           </tfoot>
@@ -54,12 +38,19 @@ class ShoppingCart extends Component {
     )
   }
 
-  removeFromCart(product) {
-    store.dispatch({
-      type: "REMOVE_FROM_CART",
-      product: product
-    });
+}
+
+const mapStateToProps = state => {
+  return {
+    cart: state.cart
   }
 }
 
-export default ShoppingCart;
+const mapDispatchToProps = dispatch => {
+  return {
+    removeFromCart(product) {
+      dispatch(removeFromCart(product));
+    }
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps) (ShoppingCart);
